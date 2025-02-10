01e165a48c36fe5d6c947ec9c2a6936af521ad2d
Paresh-ch-Pothal <pareshchandrapothal44@gmail.com>
2025-02-11 01:36:56 +0530

    updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { MdOutlineLightMode } from "react-icons/md";
 import { IoMoon } from "react-icons/io5";
 import { Link, useNavigate } from 'react-router-dom';
+import image from "../assets/image.png"
 
 const Navbar = (props) => {
 
@@ -21,9 +22,9 @@ const Navbar = (props) => {
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="/">
-                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" />
+                        <img src={image} alt="" width="70" height="43" />
                     </a>
-                    <a className="navbar-brand" href="/">Navbar</a>
+                    <a className="navbar-brand" href="/">Code Runner</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
